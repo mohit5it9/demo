d93732052581f66de07e994e029c76c7b62a4676
Mohit <mmarwadi@gmail.com>
2016-03-17 01:36:33 +0530

    added usernames

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ app.use(express.static(__dirname + '/public'));
 var connectedUsers = 0;
 
 io.on('connection', function(socket){
-	console.log(socket.id,socket.rooms);
+	console.log(socket.id);
 	connectedUsers++;
 	io.emit('chat connections',connectedUsers);
-  	socket.on('chat message', function(msg){
-   	io.emit('emit message', msg);
+  	socket.on('chat message', function(user,msg){
+   	io.emit('emit message', user,msg);
   	});
   	socket.on('typing event',function(data){
+      console.log(data);
   		socket.broadcast.emit('typing event',data);
   	});
   	socket.on('disconnect',function(){
